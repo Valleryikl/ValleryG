bba94d91947a4722291911682a2ad1ce9decf6e0
ValeriaG <valeria.girtie@epitech.eu>
2025-05-09 10:25:48 +0200

    add: a little information about myself

---
+++ frontend/src/pag/about.tsx
@@ -0,0 +1,17 @@
+import Nav from "../components/nav";
+export default function About() {
+    return (
+        <>
+            <Nav />
+            <section className="section-about" id="About">
+                <div className="about">
+                    <h4>My story</h4>
+                    <p>I discovered programming at 14, fascinated by the magic it seemed to hold. Despite early
+                        obstacles like coding on an old, faulty laptop and eventually my phone, my passion only grew stronger.
+                        Exploring various areas and languages led me to web development, and ultimately to pursuing a career as a
+                        Full-stack developer at Epitech's Web@cademie. Now, at 19, my enthusiasm for coding continues to inspire me daily.</p>
+                </div>
+            </section>
+        </>
+    )
+}
